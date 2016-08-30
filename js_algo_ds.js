f3555e58f6866c305b35beb5a5ee2eda718715da
echosteg <email@echosteg.com>
2016-08-30 22:32:00 +0530

    fibonacci number and series

--- js_algo_ds.js
+++ js_algo_ds.js
@@ -1,3 +1,29 @@
-/**
- * Created by echosteg on 28/08/2016.
- */
+/** Fibonacci number (F10 = 55) **/
+let fibonacciNumber = index => {
+  let a=0,b=1,temp;
+  while (index>1){
+    temp=b;
+    b=b+a;
+    a=temp;
+    index--;
+  }
+  return b;
+};
+
+console.log('Fibonacci Number: '+fibonacciNumber(10));
+
+/** Fibonacci series (Fn = Fn-1 + Fn-2) **/
+let fibonacciSeries = index => {
+  let a=0,b=1,temp,fbArray=[0,1];
+  while (index>1){
+    temp=b;
+    b=b+a;
+    a=temp;
+    fbArray.push(b);
+    index--;
+  }
+  return fbArray.join(',');
+};
+
+console.log('Fibonacci Series: '+fibonacciSeries(10));
+
